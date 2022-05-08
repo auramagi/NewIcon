e611a28a4a51ccb9e18f8eeea8d3139676ff3492
Mikhail Apurin <mike.apurin@gmail.com>
2022-05-08 21:34:30 +0900

    Set icon from template

--- Sources/NewIcon/Subcommands/TextCommand.swift
+++ Sources/NewIcon/Subcommands/TextCommand.swift
@@ -27,11 +27,19 @@ struct TextCommand: ParsableCommand {
     )
     var text: String
     
+    @Option(
+        name: .shortAndLong,
+        help: "Path to template file",
+        completion: .file()
+    )
+    var template: String?
     
     func run() throws {        
         guard FileManager.default.fileExists(atPath: path) else {
-            throw "File does not exist"
+            throw "File does not exist at path \(path)"
         }
+        
+        let template = try prepareTemplate()
 
         let workspace = NSWorkspace.shared
         
@@ -41,38 +49,95 @@ struct TextCommand: ParsableCommand {
         let originalIcon = workspace.icon(forFile: path)
         
         do {
-            try setNewIcon(originalIcon: originalIcon, workspace: workspace)
+            let bestRepresentation = try originalIcon.bestRepresentation(for: .infinite, context: nil, hints: [:])
+                .unwrapOrThrow("Could not convert the original icon")
+            
+            let originalIcon = NSImage()
+            originalIcon.addRepresentation(bestRepresentation)
+            
+            let view = try template.render(originalIcon, text)
+            let newIcon = try view
+                .frame(width: 1024, height: 1024)
+                .colorScheme(.light)
+                .asNSImage()
+            workspace.setIcon(newIcon, forFile: path)
+            try template.cleanUp()
         } catch {
             workspace.setIcon(oldIcon, forFile: path)
+            try? template.cleanUp()
             throw error
         }
     }
     
-    private func setNewIcon(originalIcon: NSImage, workspace: NSWorkspace) throws {
-        let bestRepresentation = try originalIcon.bestRepresentation(for: .infinite, context: nil, hints: [:])
-            .unwrapOrThrow("Could not convert the original icon")
-        
-        let originalIcon = NSImage()
-        originalIcon.addRepresentation(bestRepresentation)
-        
-        let newIcon = try IconTextView(icon: originalIcon, text: text).asNSImage()
-        
-        workspace.setIcon(newIcon, forFile: path)
+    private func prepareTemplate() throws -> Template {
+        if let template = template {
+            return try buildTemplate()
+        } else {
+            return .init(
+                render: { AnyView(IconTextView(image: $0, text: $1)) },
+                cleanUp: { }
+            )
+        }
+    }
+    
+    private func buildTemplate() throws -> Template {
+        let plugin = try TemplatePlugin()
+        do {
+            let pluginURL = try plugin.build()
+            guard let pluginHandle = dlopen(pluginURL.path, RTLD_NOW) else {
+                if let error = dlerror() {
+                    throw "dlopen error: \(String(cString: error))"
+                } else {
+                    throw "Unknown dlopen error"
+                }
+            }
+            
+            let type = try dlsym(pluginHandle, "$s8Template12IconTextViewVN")
+                .map { unsafeBitCast($0, to: Any.Type.self) }
+                .unwrapOrThrow("Plugin doesn't contain a template struct")
+            
+            typealias MakeTemplate = @convention(c) (Any, NSImage, String) -> Any
+            let renderTemplate = try dlsym(pluginHandle, "renderTemplate")
+                .map { unsafeBitCast($0, to: (MakeTemplate).self) }
+                .unwrapOrThrow("Plugin doesn't contain the makeTemplate entry point from TemplateSupport")
+            
+            return .init(
+                render: { i, _ in
+                    try (renderTemplate(type, i, "YEE") as? AnyView)
+                        .unwrapOrThrow("Could not generate SwiftUI view from plugin")
+                },
+                cleanUp: {
+                    try plugin.cleanUp()
+                }
+            )
+        } catch {
+            try? plugin.cleanUp()
+            throw error
+        }
     }
 }
 
+private extension TextCommand {
+    struct Template {
+        let render: (_ image: NSImage, _ text: String) throws -> AnyView
+        let cleanUp: () throws -> Void
+    }
+}
+
+// Default template. Synced to PluginTemplate/Template-Template-swift.template
 private struct IconTextView: View {
-    let icon: NSImage
-    
+    let image: NSImage
+
     let text: String
-    
+
+    // Expect size to be 1024x1024
     var body: some View {
-        Image(nsImage: icon)
+        Image(nsImage: image)
             .resizable()
-            .frame(width: 1024, height: 1024)
             .overlay(
                 Text(text)
                     .font(.system(size: 160, weight: .bold, design: .rounded))
+                    .colorScheme(.dark)
                     .minimumScaleFactor(0.1)
                     .multilineTextAlignment(.center)
                     .frame(width: 612)
@@ -84,6 +149,5 @@ private struct IconTextView: View {
                     )
                     .alignmentGuide(VerticalAlignment.center) { $0.height / 2 - 184 }
             )
-            .environment(\.colorScheme, .dark)
     }
 }
